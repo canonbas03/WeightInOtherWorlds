bfa1d1533f18a276e204fd2c300499fa5403a0e8
canonbas03 <166402031+canonbas03@users.noreply.github.com>
2025-03-12 19:51:17 +0200

    Rotate planets on swipe

--- Assets/Scripts/RotateScroll.cs
+++ Assets/Scripts/RotateScroll.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RotateScroll : MonoBehaviour
 {
+    public float speed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,13 @@ public class RotateScroll : MonoBehaviour
         if(Input.touchCount == 1)
         {
             Debug.Log("Touched!");
+
+            Touch screenTouch = Input.GetTouch(0);
+
+            if(screenTouch.phase == TouchPhase.Moved)
+            {
+                transform.Rotate(0f, 0f, -screenTouch.deltaPosition.x*speed);
+            }
         }
     }
 }
