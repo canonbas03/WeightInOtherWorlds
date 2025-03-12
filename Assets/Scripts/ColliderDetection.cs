d7a01f40a16a4fd0825febd18ae209db72bf8a45
canonbas03 <166402031+canonbas03@users.noreply.github.com>
2025-03-12 20:40:26 +0200

    Size change of active planets
    When a planet is in the middle it changes color (gets highlighted) and size

--- Assets/Scripts/ColliderDetection.cs
+++ Assets/Scripts/ColliderDetection.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class ColliderDetection : MonoBehaviour
 {
     private SpriteRenderer sRenderer;
+    private Transform mTransform;
 
     void Start()
     {
         sRenderer = GetComponent<SpriteRenderer>();
-       
+        mTransform = GetComponent<Transform>();
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Detector")
         {
             sRenderer.color = Color.white;
+            mTransform.localScale = new Vector3(mTransform.localScale.x, mTransform.localScale.y, mTransform.localScale.z)*2;
         }
-       
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
-            sRenderer.color = new Color(0.4f, 0.4f, 0.4f, 1f);
-        
+
+        sRenderer.color = new Color(0.4f, 0.4f, 0.4f, 1f);
+        mTransform.localScale = new Vector3(mTransform.localScale.x, mTransform.localScale.y, mTransform.localScale.z) / 2;
     }
 }
