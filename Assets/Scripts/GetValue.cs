b1da7f84a793c232c64ed34704ed067edbfd60a8
canonbas03 <166402031+canonbas03@users.noreply.github.com>
2025-03-17 19:52:31 +0200

    Fixed int conversion
    replaced TextMeshProUGUI with TMP_InputField to prevent hidden characters disturbing the parsing to int

--- Assets/Scripts/GetValue.cs
+++ Assets/Scripts/GetValue.cs
@@ -7,11 +7,13 @@ using UnityEngine.UIElements;
 
 public class GetValue : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI myText;
+    [SerializeField] TMP_InputField myText;
     public void KeepValue()
     {
         int dataToKeep;
         int.TryParse(myText.text, out dataToKeep);
+        Debug.Log($"Input Text: '{myText.text}'");
+
         Debug.Log(dataToKeep);
         StaticData.valueToKeep = dataToKeep;
 
