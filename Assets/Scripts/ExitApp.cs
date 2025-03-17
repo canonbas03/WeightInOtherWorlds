e71728bfa560e3b77e94bef17f61291d1542d412
canonbas03 <166402031+canonbas03@users.noreply.github.com>
2025-03-17 20:05:10 +0200

    exit app on back button

---
+++ Assets/Scripts/ExitApp.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApp : MonoBehaviour
+{
+
+    void Start()
+    {
+        Input.backButtonLeavesApp = true;
+    }
+}
+
