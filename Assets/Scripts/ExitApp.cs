4767693ce2cf2638206181fc27174af8d6beae35
canonbas03 <166402031+canonbas03@users.noreply.github.com>
2025-03-28 18:38:47 +0200

    Updated back key functionality
    Depending on which scene you are the back button triggers the corresponding action.

--- Assets/Scripts/ExitApp.cs
+++ Assets/Scripts/ExitApp.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitApp : MonoBehaviour
 {
-
-    void Start()
+    private void Update()
     {
-        Input.backButtonLeavesApp = true;
+        if (Input.GetKeyDown(KeyCode.Escape))  // Escape is back key in mobile by defoult
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0) // 0 is Main Scene
+                Application.Quit();
+            else { SceneManager.LoadScene(0); }
+        }
     }
 }
 
