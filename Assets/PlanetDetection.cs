52a88bcfd104f7e54ba30e03149f4489997fa047
canonbas03 <166402031+canonbas03@users.noreply.github.com>
2025-04-03 17:48:48 +0300

    Migration of Planet Detection script
    Before each planet had a script for being detected, now there is only one detection script attached to the Detector

--- Assets/PlanetDetection.cs
+++ Assets/PlanetDetection.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class ColliderDetection : MonoBehaviour
+public class PlanetDetection : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI textToModify;
     AudioSource AudioSource;
@@ -11,86 +11,103 @@ public class ColliderDetection : MonoBehaviour
     private SpriteRenderer sRenderer;
     private Transform mTransform;
 
-
+    public GameManager gameManager;
     void Start()
     {
-        sRenderer = GetComponent<SpriteRenderer>();
-        mTransform = GetComponent<Transform>();
+       
 
         AudioSource = GameObject.FindGameObjectWithTag("Detector").GetComponent<AudioSource>();
         AudioClips = GameObject.FindGameObjectWithTag("Detector").GetComponent<MultipleAudio>().audioClips;
 
 
     }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if(collision.gameObject.tag == "Earth")
+    //    {
+    //        Debug.Log("fOUND LIFE");
+    //    }
+    //}
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        sRenderer = collision.GetComponent<SpriteRenderer>();
+        mTransform = collision.GetComponent<Transform>();
+
         int earthWeight = StaticData.valueToKeep;
-        if (collision.gameObject.tag == "Detector")
-        {
+       
+        
             PlayAudio();
 
             sRenderer.color = Color.white;
             mTransform.localScale = new Vector3(mTransform.localScale.x, mTransform.localScale.y, mTransform.localScale.z) * 2;
 
+           
 
-            if (gameObject.tag == "Earth")
+            if (collision.gameObject.tag == "Earth")
             {
+                sRenderer = collision.GetComponent<SpriteRenderer>();
+                mTransform = collision.GetComponent<Transform>();
+
                 textToModify.text = $"On Earth you weight {earthWeight} kg. Unbelievable, right?";
             }
-            if (gameObject.tag == "Mercury")
+            if (collision.gameObject.tag == "Mercury")
             {
+                sRenderer = collision.GetComponent<SpriteRenderer>();
+                mTransform = collision.GetComponent<Transform>();
+
                 float mercuryWeight = earthWeight * 0.38f;
                 textToModify.text = $"On Mercury you weigh {mercuryWeight} kg. Light as a feather!";
             }
-            if (gameObject.tag == "Venera")
+            if (collision.gameObject.tag == "Venera")
             {
                 float veneraWeight = earthWeight * 0.91f;
                 textToModify.text = $"On Venus you weigh {veneraWeight} kg. Heavy and hot!";
             }
-            if (gameObject.tag == "Mars")
+            if (collision.gameObject.tag == "Mars")
             {
                 float marsWeight = earthWeight * 0.38f;
                 textToModify.text = $"On Mars you weigh {marsWeight} kg. Time to bounce around!";
             }
-            if (gameObject.tag == "Jupiter")
+            if (collision.gameObject.tag == "Jupiter")
             {
                 float jupiterWeight = earthWeight * 2.34f;
                 textToModify.text = $"On Jupiter you weigh {jupiterWeight} kg. Feeling super heavy!";
             }
-            if (gameObject.tag == "Saturn")
+            if (collision.gameObject.tag == "Saturn")
             {
                 float saturnWeight = earthWeight * 0.93f;
                 textToModify.text = $"On Saturn you weigh {saturnWeight} kg. Rings can't hold you down!";
             }
-            if (gameObject.tag == "Uran")
+            if (collision.gameObject.tag == "Uran")
             {
                 float uranWeight = earthWeight * 0.92f;
                 textToModify.text = $"On Uranus you weigh {uranWeight} kg. Ice cold and light!";
             }
-            if (gameObject.tag == "Neptune")
+            if (collision.gameObject.tag == "Neptune")
             {
                 float neptuneWeight = earthWeight * 1.12f;
                 textToModify.text = $"On Neptune you weigh {neptuneWeight} kg. Deep blue and dense!";
             }
 
-           
 
-        }
+
+        
 
     }
-
     private void OnTriggerExit2D(Collider2D collision)
     {
-
+        sRenderer = collision.GetComponent<SpriteRenderer>();
+        mTransform = collision.GetComponent<Transform>();
         sRenderer.color = new Color(0.4f, 0.4f, 0.4f, 1f);
         mTransform.localScale = new Vector3(mTransform.localScale.x, mTransform.localScale.y, mTransform.localScale.z) / 2;
     }
 
     void PlayAudio()
     {
-      
+
         int selected = Random.Range(0, AudioClips.Length);
         AudioSource.PlayOneShot(AudioClips[selected]);
     }
-
 }
